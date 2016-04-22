061b5bd1a625a701f781c9eb13847f1decaab2bc
maquieirajoseluis <maquieirajoseluis@gmail.com>
2016-04-22 00:10:23 -0300

    Use netNamedPipeBinding

--- WcfGettingStartedHost/Program.cs
+++ WcfGettingStartedHost/Program.cs
@@ -14,7 +14,7 @@ namespace WcfGettingStartedHost
         static void Main(string[] args)
         {
             // Step 1 Create a URI to serve as the base address.
-            Uri baseAddress = new Uri("http://localhost:8733/WcfGettingStartedTutorial/");
+            Uri baseAddress = new Uri("net.pipe://localhost/WcfGettingStartedTutorial/");
 
             // Step 2 Create a ServiceHost instance
             ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
@@ -22,12 +22,10 @@ namespace WcfGettingStartedHost
             try
             {
                 // Step 3 Add a service endpoint.
-                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new NetNamedPipeBinding(), "CalculatorService");
 
                 // Step 4 Enable metadata exchange.
                 ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-                smb.HttpGetEnabled = true;
-                selfHost.Description.Behaviors.Add(smb);
 
                 // Step 5 Start the service.
                 selfHost.Open();
